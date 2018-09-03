532f07d160df9e57e2c3a21e2e5dd48af3f60236
unknown <1169470657@qq.com>
2018-09-04 01:21:58 +0800

    first commit

---
+++ config/config.default.js
@@ -0,0 +1,30 @@
+'use strict';
+const path = require('path');
+module.exports = appInfo => {
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1535988718693_5908';
+
+  // add your config here
+  config.middleware = [];
+
+  exports.mongoose = {
+    url: 'mongodb://127.0.0.1/prize-form-back',
+    options: {}
+  };
+
+  config.security = {
+    csrf: {
+      enable: false,
+    },
+  };
+
+  config.static = {
+    prefix: '/',
+    dir: path.join(appInfo.baseDir, 'app/public'),
+    dynamic: true,
+  };
+
+  return config;
+};
